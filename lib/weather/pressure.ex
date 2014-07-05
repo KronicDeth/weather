3fdbe6b8a057068fe6c8a55c42e14e77d2db8e25
Luke Imhoff <Kronic.Deth@gmail.com>
2014-07-04 23:01:23 -0500

    Add docs for Weather.Pressure

--- lib/weather/pressure.ex
+++ lib/weather/pressure.ex
@@ -1,14 +1,29 @@
 defmodule Weather.Pressure do
+  @moduledoc """
+  A pressure's `magnitude` along with the `units` of the magnitude.  Supported
+  `units` are `"in Hg" for inches mercury and `"mb"` for millibars.
+
+  Pressures can be converted between the two units using the conversion
+  function: `to_in_hg/1`, `to_mb/1`.  Conversion functions called on pressures
+  already in the target `units` return the given pressure.
+  """
+
   defstruct magnitude: 0, units: "in Hg"
 
   def to_in_hg(pressure = %Weather.Pressure{units: "in Hg"}) do
     pressure
   end
 
+  @doc """
+  Returns the pressure converted to in Hg.
+  """
   def to_in_hg(%Weather.Pressure{magnitude: millibars, units: "mb"}) do
     %Weather.Pressure{magnitude: millibars * 0.02955301, units: "in Hg"}
   end
 
+  @doc """
+  Returns the pressure converted to mb.
+  """
   def to_mb(%Weather.Pressure{magnitude: in_hg, units: "in Hg"}) do
     %Weather.Pressure{magnitude: in_hg * 33.8637526, units: "mb"}
   end
