7697db9b73db5f105ca0171686353698a6075100
Luke Imhoff <Kronic.Deth@gmail.com>
2014-07-05 22:23:53 -0500

    Weather.Wind docs

--- lib/weather/wind.ex
+++ lib/weather/wind.ex
@@ -1,4 +1,8 @@
 defmodule Weather.Wind do
+  @moduledoc """
+  The wind heading in `degrees`, `direction`, and `speed`.
+  """
+
   defstruct degrees: 90.0 :: float,
             direction: "East" :: String.t,
             speed: %Weather.Speed{} :: Weather.Speed.t
